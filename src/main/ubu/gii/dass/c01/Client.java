7ff597e6f22b30dfdb69e9d34e3799e1c3bec41c
rnc0011 <rnc0011@alu.ubu.es>
2018-02-22 19:04:00 +0100

    Se han quitado comentarios de pruebas anteriores.

--- src/main/ubu/gii/dass/c01/Client.java
+++ src/main/ubu/gii/dass/c01/Client.java
@@ -2,33 +2,27 @@ package ubu.gii.dass.c01;
 
 import java.util.logging.Logger;
 
-//Probando... Probando... Probando mi amor por ti!!!!!!!!!!!
-public class Client{
-	
-	
-	public static void main(String arg[]) throws NotFreeInstanceException, DuplicatedInstanceException{
-	  ReusablePool pool;
-	  Reusable r1,r2,r3;
-	  Logger logger = Logger.getLogger("c01");
-	 
-	  
-	  pool = ReusablePool.getInstance();
-	  r1 = pool.acquireReusable();
-	  r2 = pool.acquireReusable();
-	  
-	 
-	  logger.info(r1.util());
-	  logger.info(r2.util());
-	   
-	
-	  	  
-	  pool.releaseReusable(r2);
-	  r3= pool.acquireReusable();
-	  
-	  logger.info(r3.util());
-	  
-	  logger.info(r1.util());
-	
+public class Client {
+
+	public static void main(String arg[]) throws NotFreeInstanceException, DuplicatedInstanceException {
+		ReusablePool pool;
+		Reusable r1, r2, r3;
+		Logger logger = Logger.getLogger("c01");
+
+		pool = ReusablePool.getInstance();
+		r1 = pool.acquireReusable();
+		r2 = pool.acquireReusable();
+
+		logger.info(r1.util());
+		logger.info(r2.util());
+
+		pool.releaseReusable(r2);
+		r3 = pool.acquireReusable();
+
+		logger.info(r3.util());
+
+		logger.info(r1.util());
+
 	}
 
-} 
+}
